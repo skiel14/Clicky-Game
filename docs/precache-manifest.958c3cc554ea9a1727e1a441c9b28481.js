292f4b4bf2ce9dd101d5c3655538887e9c7b67e6
skiel <platypony14@gmail.com>
2019-07-06 10:09:38 -0400

    added docs folder

---
+++ docs/precache-manifest.958c3cc554ea9a1727e1a441c9b28481.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14b8d472dc9b4b0bdb7e1796825405ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e1f635b81a6c8c29036",
+    "url": "/static/css/main.9c36dbe2.chunk.css"
+  },
+  {
+    "revision": "9508b3e097f5d4db1edd",
+    "url": "/static/js/2.97807068.chunk.js"
+  },
+  {
+    "revision": "8e1f635b81a6c8c29036",
+    "url": "/static/js/main.aa2dcb97.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c618d7874afaa5a7d337f0c687d7b1d3",
+    "url": "/static/media/baby-bear.c618d787.jpg"
+  },
+  {
+    "revision": "6590bea51456f8f050ac345884019de5",
+    "url": "/static/media/baby-deer.6590bea5.jpg"
+  },
+  {
+    "revision": "3433f0ee236bcac9c638f1957c67ebe0",
+    "url": "/static/media/baby-ducks.3433f0ee.jpg"
+  },
+  {
+    "revision": "3837600cd7d905b8a41ea935bb629b5e",
+    "url": "/static/media/baby-giraffe.3837600c.jpg"
+  },
+  {
+    "revision": "bbe5cca4977bb909e325a72d6ff1a9aa",
+    "url": "/static/media/baby-hedgehog.bbe5cca4.jpg"
+  },
+  {
+    "revision": "6542063018a80d91a3085650dedecfd3",
+    "url": "/static/media/baby-lion.65420630.jpg"
+  },
+  {
+    "revision": "aca4c671e2dad132245bbbeea1245bdc",
+    "url": "/static/media/baby-rabbit.aca4c671.jpg"
+  },
+  {
+    "revision": "eaca1a885204c3254d6512f372a2ef96",
+    "url": "/static/media/baby-seal.eaca1a88.jpg"
+  },
+  {
+    "revision": "6b7bb6c8822e3d3ef69701f8acb58546",
+    "url": "/static/media/baby-turtle.6b7bb6c8.jpg"
+  },
+  {
+    "revision": "f6d86a2cbbf694098b283cf96509d43f",
+    "url": "/static/media/kitten.f6d86a2c.jpg"
+  },
+  {
+    "revision": "938195d0395a6402a18c632dc83af660",
+    "url": "/static/media/lamb.938195d0.jpg"
+  },
+  {
+    "revision": "aa62df9c7eed49c1cd166c3f4bfee3e1",
+    "url": "/static/media/puppy.aa62df9c.jpg"
+  }
+]);
